3d1965a4f0a713c76fb4f2b2030bc575eebe09ab
Fran Navas <navasfran98@gmail.com>
2019-06-05 00:25:50 -0600

    nuevo sql

--- agenda-hdp115.sql
+++ agenda-hdp115.sql
@@ -1,13 +1,15 @@
-﻿-- phpMyAdmin SQL Dump
--- version 4.6.5.2
+-- phpMyAdmin SQL Dump
+-- version 4.8.4
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 04-06-2019 a las 06:15:10
--- Versión del servidor: 10.1.21-MariaDB
--- Versión de PHP: 7.1.2
+-- Tiempo de generación: 05-06-2019 a las 08:25:20
+-- Versión del servidor: 10.1.37-MariaDB
+-- Versión de PHP: 7.3.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -40,10 +42,15 @@ CREATE TABLE `avatar` (
 --
 
 INSERT INTO `avatar` (`id_avatar`, `nombre`, `avatar_url`, `tipo`, `created_at`, `updated_at`) VALUES
-(1, 'anime 1', 'http://127.0.0.1:8000/AVATAR/vIWyqP3Z3D7rp6EbnXn0oQ4hZZej2kncMdIu7qyb.jpeg', 'publico', '2019-05-11 07:00:47', '0000-00-00 00:00:00'),
-(2, 'anime 2', 'http://127.0.0.1:8000/AVATAR/xLrKdyWzvX1yytsiWFWy6wFqM8MBgcXchDPWEqYo.png', 'publico', '2019-05-11 07:03:01', '2019-05-11 07:03:01'),
-(3, 'Pikachu', 'http://mobile-yetitask.djfrankremixer.com/avatars/QwV5EqgMVFns4FvLdkun2usUM5DwczXdBVi2clrV.gif', 'wi3D502F400lsMU592F', '2019-05-11 07:03:01', '2019-05-11 07:03:01'),
-(4, 'Anime 4', 'http://mobile-yetitask.djfrankremixer.com/avatars/14fLsM3tR4mlTuhX5VNVmoQT4yg8DLdYbjkSNKxe.jpeg', 'JDwC86aE69pbWJR19aE', '2019-05-11 07:03:01', '2019-05-11 07:03:01');
+(1, 'anime 1', 'http://127.0.0.1:8000/AVATAR/vIWyqP3Z3D7rp6EbnXn0oQ4hZZej2kncMdIu7qyb.jpeg', 'publico', '2019-05-11 13:00:47', '0000-00-00 00:00:00'),
+(2, 'anime 2', 'http://127.0.0.1:8000/AVATAR/xLrKdyWzvX1yytsiWFWy6wFqM8MBgcXchDPWEqYo.png', 'publico', '2019-05-11 13:03:01', '2019-05-11 13:03:01'),
+(3, 'Task', 'http://127.0.0.1:8000/AVATAR/OPVLTOakMYTdYLCze1fSPLQqeDUnbn5IcxJB8E2p.png', 'publico', '2019-06-05 12:07:51', '2019-06-05 12:07:51'),
+(4, 'Anime 4', 'http://mobile-yetitask.djfrankremixer.com/avatars/14fLsM3tR4mlTuhX5VNVmoQT4yg8DLdYbjkSNKxe.jpeg', 'JDwC86aE69pbWJR19aE', '2019-05-11 13:03:01', '2019-05-11 13:03:01'),
+(5, 'Pikachu', 'http://mobile-yetitask.djfrankremixer.com/avatars/QwV5EqgMVFns4FvLdkun2usUM5DwczXdBVi2clrV.gif', 'wi3D502F400lsMU592F', '2019-05-11 13:03:01', '2019-05-11 13:03:01'),
+(9, 'Zero two', 'http://127.0.0.1:8000/AVATAR/BjtF0AiIx4QhloQNG68MA5mrXCW9USwMwGssQuTZ.png', 'wi3D502F400lsMU592F', '2019-06-05 11:17:55', '2019-06-05 11:17:55'),
+(10, 'Anime 4', 'http://127.0.0.1:8000/AVATAR/BdSP8Vm3ZkYpo6U3BktvFsLmHXU8DNNk2jGExvLX.jpeg', 'wi3D502F400lsMU592F', '2019-06-05 11:18:13', '2019-06-05 11:18:13'),
+(11, 'Zombie', 'http://127.0.0.1:8000/AVATAR/Pc3dH3fKW35BAoGZAnMYnV3AVYgDjbJRadtTua4h.png', 'wi3D502F400lsMU592F', '2019-06-05 11:18:30', '2019-06-05 11:18:30'),
+(12, 'Bola', 'http://127.0.0.1:8000/AVATAR/qtpdBFEJs0eqWSyU3V2Uosh9yujHEwfApq5PaQX1.png', 'wi3D502F400lsMU592F', '2019-06-05 11:24:17', '2019-06-05 11:24:17');
 
 -- --------------------------------------------------------
 
@@ -64,8 +71,8 @@ CREATE TABLE `grupo` (
 --
 
 INSERT INTO `grupo` (`codigo_grupo`, `nombre_grupo`, `descripcion`, `created_at`, `updated_at`) VALUES
-('JDwC86aE69pbWJR19aE', 'Grupo Hogar FPE', 'No tiene', '2019-06-01 11:49:56', '2019-06-01 11:49:56'),
-('wi3D502F400lsMU592F', 'TareaHDP115', 'Grupo para gestionar la tarea de Herramientas de productividad', '2019-06-01 05:49:05', '2019-06-01 05:48:33');
+('JDwC86aE69pbWJR19aE', 'Grupo Hogar FPE', 'No tiene', '2019-06-01 17:49:56', '2019-06-01 17:49:56'),
+('wi3D502F400lsMU592F', 'TareaHDP115', 'Grupo para gestionar la tarea de Herramientas de productividad', '2019-06-01 11:49:05', '2019-06-01 11:48:33');
 
 -- --------------------------------------------------------
 
@@ -87,9 +94,9 @@ CREATE TABLE `grupo_user` (
 --
 
 INSERT INTO `grupo_user` (`id_grupo_user`, `user_id`, `codigo_grupo`, `rol`, `created_at`, `updated_at`) VALUES
-(1, 2, 'wi3D502F400lsMU592F', 'Administrador', '2019-06-01 05:49:22', '2019-06-01 05:49:52'),
-(2, 3, 'JDwC86aE69pbWJR19aE', 'Administrador', '2019-06-01 11:49:56', '2019-06-01 11:49:56'),
-(6, 9, 'wi3D502F400lsMU592F', 'Usuario', '2019-06-03 10:06:34', '2019-06-03 10:06:34');
+(1, 2, 'wi3D502F400lsMU592F', 'Administrador', '2019-06-01 11:49:22', '2019-06-01 11:49:52'),
+(2, 3, 'JDwC86aE69pbWJR19aE', 'Administrador', '2019-06-01 17:49:56', '2019-06-01 17:49:56'),
+(6, 9, 'wi3D502F400lsMU592F', 'Usuario', '2019-06-03 16:06:34', '2019-06-03 16:06:34');
 
 -- --------------------------------------------------------
 
@@ -146,10 +153,10 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `grupo_activo`, `avatar`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'Juan Barrera', 'barrera98@gmail.com', '$2y$10$dYwnj637NoJT027pMZR8o.aXYhRT5R5URt6CY9X1IACxPiSYm7Rqe', 'owner', 'https://i.ibb.co/P5p1trd/contacts2.png', 'TYdnLUdj4XGIwKj3Zqm9Ij0vPwdMjIt2ETUhvqYNooc1RZq8ni75Vk8GKkXt', '2019-05-29 11:22:35', '2019-05-29 11:22:35'),
-(2, 'Francisco Navas', 'navasfran98@gmail.com', '$2y$10$dYwnj637NoJT027pMZR8o.aXYhRT5R5URt6CY9X1IACxPiSYm7Rqe', 'wi3D502F400lsMU592F', 'AVATAR/vIWyqP3Z3D7rp6EbnXn0oQ4hZZej2kncMdIu7qyb.jpeg', 'TYdnLUdj4XGIwKj3Zqm9Ij0vPwdMjIt2ETUhvqYNooc1RZq8ni75Vk8GKkXt', '2019-05-29 11:22:35', '2019-05-29 11:22:35'),
-(3, 'karla Margarita', 'karla@gmail.com', '$2y$10$AwOP.qc5u5go08GSvRP.MO.BTUmbCDNsw.UF.ErLtk7dpMwmjcFoe', 'JDwC86aE69pbWJR19aE', 'https://i.ibb.co/P5p1trd/contacts2.png', 'WJtdU1XmwSl1OipsIGR7wcawPrvlfZOkFD2JJ74KgX099ED5hojJSHM9u0gH', '2019-06-01 11:49:56', '2019-06-01 11:49:56'),
-(9, 'Carlos Canela', 'carloscanela@gmail.com', '$2y$10$6DsaaraJtyJ0Or6B.3Zj8uk.YoJklmvqpPGwIfOwXSPsZbAmmZhOm', 'wi3D502F400lsMU592F', 'AVATAR/xLrKdyWzvX1yytsiWFWy6wFqM8MBgcXchDPWEqYo.png', NULL, '2019-06-03 10:06:34', '2019-06-03 10:06:34');
+(1, 'Juan Barrera', 'barrera98@gmail.com', '$2y$10$dYwnj637NoJT027pMZR8o.aXYhRT5R5URt6CY9X1IACxPiSYm7Rqe', 'owner', 'https://i.ibb.co/P5p1trd/contacts2.png', 'TYdnLUdj4XGIwKj3Zqm9Ij0vPwdMjIt2ETUhvqYNooc1RZq8ni75Vk8GKkXt', '2019-05-29 17:22:35', '2019-05-29 17:22:35'),
+(2, 'Francisco Navas', 'navasfran98@gmail.com', '$2y$10$dYwnj637NoJT027pMZR8o.aXYhRT5R5URt6CY9X1IACxPiSYm7Rqe', 'wi3D502F400lsMU592F', 'http://127.0.0.1:8000/AVATAR/vIWyqP3Z3D7rp6EbnXn0oQ4hZZej2kncMdIu7qyb.jpeg', 'L5TSvMaZSGCeHIEN928ior8DDH7ac6s6HlwgQ6JlZKro9TKesJyX6cun4EP1', '2019-05-29 17:22:35', '2019-06-05 11:54:03'),
+(3, 'karla Margarita', 'karla@gmail.com', '$2y$10$AwOP.qc5u5go08GSvRP.MO.BTUmbCDNsw.UF.ErLtk7dpMwmjcFoe', 'JDwC86aE69pbWJR19aE', 'https://i.ibb.co/P5p1trd/contacts2.png', 'WJtdU1XmwSl1OipsIGR7wcawPrvlfZOkFD2JJ74KgX099ED5hojJSHM9u0gH', '2019-06-01 17:49:56', '2019-06-01 17:49:56'),
+(9, 'Carlos Canela', 'carloscanela@gmail.com', '$2y$10$6DsaaraJtyJ0Or6B.3Zj8uk.YoJklmvqpPGwIfOwXSPsZbAmmZhOm', 'wi3D502F400lsMU592F', 'AVATAR/xLrKdyWzvX1yytsiWFWy6wFqM8MBgcXchDPWEqYo.png', NULL, '2019-06-03 16:06:34', '2019-06-03 16:06:34');
 
 --
 -- Índices para tablas volcadas
@@ -202,22 +209,26 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT de la tabla `avatar`
 --
 ALTER TABLE `avatar`
-  MODIFY `id_avatar` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_avatar` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
 --
 -- AUTO_INCREMENT de la tabla `grupo_user`
 --
 ALTER TABLE `grupo_user`
   MODIFY `id_grupo_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
 --
 -- AUTO_INCREMENT de la tabla `migrations`
 --
 ALTER TABLE `migrations`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
 --
 -- AUTO_INCREMENT de la tabla `users`
 --
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
 --
 -- Restricciones para tablas volcadas
 --
@@ -228,6 +239,7 @@ ALTER TABLE `users`
 ALTER TABLE `grupo_user`
   ADD CONSTRAINT `grupo_user_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
   ADD CONSTRAINT `grupo_user_ibfk_2` FOREIGN KEY (`codigo_grupo`) REFERENCES `grupo` (`codigo_grupo`);
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
