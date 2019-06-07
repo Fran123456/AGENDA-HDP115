6056f87c87f9d3a97d806c3ff9aae9f47cace30a
fran <navasfran98@gmail.com>
2019-06-06 18:29:52 -0600

    sql 2

--- agenda-hdp115.sql
+++ agenda-hdp115.sql
@@ -1,29 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 4.8.4
--- https://www.phpmyadmin.net/
---
--- Servidor: 127.0.0.1
--- Tiempo de generación: 06-06-2019 a las 23:52:11
--- Versión del servidor: 10.1.37-MariaDB
--- Versión de PHP: 7.3.0
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de datos: `agenda-hdp115`
---
-
--- --------------------------------------------------------
-
+﻿
 --
 -- Estructura de tabla para la tabla `avatar`
 --
@@ -50,7 +25,8 @@ INSERT INTO `avatar` (`id_avatar`, `nombre`, `avatar_url`, `tipo`, `created_at`,
 (9, 'Zero two', 'http://127.0.0.1:8000/AVATAR/BjtF0AiIx4QhloQNG68MA5mrXCW9USwMwGssQuTZ.png', 'wi3D502F400lsMU592F', '2019-06-05 11:17:55', '2019-06-05 11:17:55'),
 (10, 'Anime 4', 'http://127.0.0.1:8000/AVATAR/BdSP8Vm3ZkYpo6U3BktvFsLmHXU8DNNk2jGExvLX.jpeg', 'wi3D502F400lsMU592F', '2019-06-05 11:18:13', '2019-06-05 11:18:13'),
 (11, 'Zombie', 'http://127.0.0.1:8000/AVATAR/Pc3dH3fKW35BAoGZAnMYnV3AVYgDjbJRadtTua4h.png', 'wi3D502F400lsMU592F', '2019-06-05 11:18:30', '2019-06-05 11:18:30'),
-(12, 'Bola', 'http://127.0.0.1:8000/AVATAR/qtpdBFEJs0eqWSyU3V2Uosh9yujHEwfApq5PaQX1.png', 'wi3D502F400lsMU592F', '2019-06-05 11:24:17', '2019-06-05 11:24:17');
+(12, 'Bola', 'http://127.0.0.1:8000/AVATAR/qtpdBFEJs0eqWSyU3V2Uosh9yujHEwfApq5PaQX1.png', 'wi3D502F400lsMU592F', '2019-06-05 11:24:17', '2019-06-05 11:24:17'),
+(14, 'Angry boy', 'http://127.0.0.1:8000/AVATAR/3FchSx0Rsbv5Ek5GqwOBLaW37gNkM3Yv7PLu6WBb.png', 'wi3D502F400lsMU592F', '2019-06-07 04:12:05', '2019-06-07 04:12:05');
 
 -- --------------------------------------------------------
 
@@ -137,6 +113,13 @@ CREATE TABLE `notificacion` (
   `updated_at` datetime DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Volcado de datos para la tabla `notificacion`
+--
+
+INSERT INTO `notificacion` (`id_noty`, `codigo_noty`, `titulo`, `cuerpo`, `creador`, `grupo`, `tarea_id`, `tipo`, `created_at`, `updated_at`) VALUES
+(2, 'Noty-Qfj23-Jf84sI682-95807sDxmyVab-2097It2', 'FRANCISCO NAVAS TE ASIGNO UNA NUEVA TAREA', 'Hola papi', 2, 'wi3D502F400lsMU592F', 'Task-Nhf84-QAH5VN742-13036ehngqrCi-3049jdV', 'tarea', '2019-06-06 23:31:27', '2019-06-06 23:31:27');
+
 -- --------------------------------------------------------
 
 --
@@ -145,12 +128,19 @@ CREATE TABLE `notificacion` (
 
 CREATE TABLE `notificacion_user` (
   `id` int(11) NOT NULL,
-  `notificacion_id` int(11) DEFAULT NULL,
+  `notificacion_id` varchar(200) DEFAULT NULL,
   `user_id` int(11) DEFAULT NULL,
   `estado` varchar(100) DEFAULT NULL,
   `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
   `updated_at` datetime DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `notificacion_user`
+--
+
+INSERT INTO `notificacion_user` (`id`, `notificacion_id`, `user_id`, `estado`, `created_at`, `updated_at`) VALUES
+(1, 'Noty-Qfj23-Jf84sI682-95807sDxmyVab-2097It2', 9, 'SIN LEER', '2019-06-06 23:31:27', '2019-06-06 23:31:27');
 
 -- --------------------------------------------------------
 
@@ -183,6 +173,13 @@ CREATE TABLE `tareas` (
   `updated_at` datetime DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Volcado de datos para la tabla `tareas`
+--
+
+INSERT INTO `tareas` (`id_tarea`, `codigo_tarea`, `titulo`, `cuerpo`, `estado`, `fecha_fin`, `creador`, `grupo`, `created_at`, `updated_at`) VALUES
+(4, 'Task-Nhf84-QAH5VN742-13036ehngqrCi-3049jdV', 'Tarea de prueba', '<p>orem ipsum dolor sit amet, consectetur adipiscing elit. Mauris facilisis vulputate urna, eu rhoncus tellus lobortis vitae. Suspendisse condimentum efficitur iaculis. Nullam posuere elit eu gravida dignissim. Donec rhoncus nulla eu mollis mattis. Nunc eleifend molestie tortor eu consequat. Donec vitae ipsum a nulla ullamcorper sagittis. Maecenas sed fermentum odio. Nam cursus dictum consequat. Fusce consectetur eget justo ut pretium. In hac habitasse platea dictumst. Vestibulum ac augue id turpis lacinia blandit vel vitae metus. Proin gravida pharetra ligula in fermentum. Morbi blandit accumsan felis, non sodales nisl. Donec suscipit, erat nec cursus egestas, sem lorem congue nibh, sed condimentum mi dolor non risus.</p>', 'Inicio', '2019-06-20', 2, 'wi3D502F400lsMU592F', '2019-06-06 23:31:26', '2019-06-06 23:31:26');
+
 -- --------------------------------------------------------
 
 --
@@ -196,6 +193,13 @@ CREATE TABLE `tareas_usuarios` (
   `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
   `updated_at` datetime DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `tareas_usuarios`
+--
+
+INSERT INTO `tareas_usuarios` (`id`, `tarea_id`, `user_id`, `created_at`, `updated_at`) VALUES
+(4, 'Task-Nhf84-QAH5VN742-13036ehngqrCi-3049jdV', 9, '2019-06-06 23:31:27', '2019-06-06 23:31:27');
 
 -- --------------------------------------------------------
 
@@ -221,7 +225,7 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `grupo_activo`, `avatar`, `remember_token`, `created_at`, `updated_at`) VALUES
 (1, 'Juan Barrera', 'barrera98@gmail.com', '$2y$10$dYwnj637NoJT027pMZR8o.aXYhRT5R5URt6CY9X1IACxPiSYm7Rqe', 'owner', 'https://i.ibb.co/P5p1trd/contacts2.png', 'TYdnLUdj4XGIwKj3Zqm9Ij0vPwdMjIt2ETUhvqYNooc1RZq8ni75Vk8GKkXt', '2019-05-29 17:22:35', '2019-05-29 17:22:35'),
-(2, 'Francisco Navas', 'navasfran98@gmail.com', '$2y$10$dYwnj637NoJT027pMZR8o.aXYhRT5R5URt6CY9X1IACxPiSYm7Rqe', 'wi3D502F400lsMU592F', 'http://127.0.0.1:8000/AVATAR/vIWyqP3Z3D7rp6EbnXn0oQ4hZZej2kncMdIu7qyb.jpeg', 'L5TSvMaZSGCeHIEN928ior8DDH7ac6s6HlwgQ6JlZKro9TKesJyX6cun4EP1', '2019-05-29 17:22:35', '2019-06-05 11:54:03'),
+(2, 'Francisco Navas', 'navasfran98@gmail.com', '$2y$10$dYwnj637NoJT027pMZR8o.aXYhRT5R5URt6CY9X1IACxPiSYm7Rqe', 'wi3D502F400lsMU592F', 'http://mobile-yetitask.djfrankremixer.com/avatars/QwV5EqgMVFns4FvLdkun2usUM5DwczXdBVi2clrV.gif', 'L5TSvMaZSGCeHIEN928ior8DDH7ac6s6HlwgQ6JlZKro9TKesJyX6cun4EP1', '2019-05-29 17:22:35', '2019-06-07 04:12:48'),
 (3, 'karla Margarita', 'karla@gmail.com', '$2y$10$AwOP.qc5u5go08GSvRP.MO.BTUmbCDNsw.UF.ErLtk7dpMwmjcFoe', 'JDwC86aE69pbWJR19aE', 'https://i.ibb.co/P5p1trd/contacts2.png', 'WJtdU1XmwSl1OipsIGR7wcawPrvlfZOkFD2JJ74KgX099ED5hojJSHM9u0gH', '2019-06-01 17:49:56', '2019-06-01 17:49:56'),
 (9, 'Carlos Canela', 'carloscanela@gmail.com', '$2y$10$6DsaaraJtyJ0Or6B.3Zj8uk.YoJklmvqpPGwIfOwXSPsZbAmmZhOm', 'wi3D502F400lsMU592F', 'AVATAR/xLrKdyWzvX1yytsiWFWy6wFqM8MBgcXchDPWEqYo.png', NULL, '2019-06-03 16:06:34', '2019-06-03 16:06:34');
 
@@ -310,50 +314,42 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT de la tabla `avatar`
 --
 ALTER TABLE `avatar`
-  MODIFY `id_avatar` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
-
+  MODIFY `id_avatar` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 --
 -- AUTO_INCREMENT de la tabla `grupo_user`
 --
 ALTER TABLE `grupo_user`
   MODIFY `id_grupo_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
-
 --
 -- AUTO_INCREMENT de la tabla `migrations`
 --
 ALTER TABLE `migrations`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
 --
 -- AUTO_INCREMENT de la tabla `notificacion`
 --
 ALTER TABLE `notificacion`
-  MODIFY `id_noty` int(11) NOT NULL AUTO_INCREMENT;
-
+  MODIFY `id_noty` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT de la tabla `notificacion_user`
 --
 ALTER TABLE `notificacion_user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT de la tabla `tareas`
 --
 ALTER TABLE `tareas`
-  MODIFY `id_tarea` int(11) NOT NULL AUTO_INCREMENT;
-
+  MODIFY `id_tarea` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT de la tabla `tareas_usuarios`
 --
 ALTER TABLE `tareas_usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT de la tabla `users`
 --
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
-
 --
 -- Restricciones para tablas volcadas
 --
@@ -364,7 +360,6 @@ ALTER TABLE `users`
 ALTER TABLE `grupo_user`
   ADD CONSTRAINT `grupo_user_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
   ADD CONSTRAINT `grupo_user_ibfk_2` FOREIGN KEY (`codigo_grupo`) REFERENCES `grupo` (`codigo_grupo`);
-COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
